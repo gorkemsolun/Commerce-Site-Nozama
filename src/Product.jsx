2ca4a71c252793bec06e03b43d1b0ff0f7fbcc94
Anfrian <gorkemkadir14@hotmail.com>
2022-07-11 05:36:27 +0300

    Shopping Cart & Product
    Finished and updated

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,15 +1,56 @@
 import React, {Component} from "react";
 
 export default class Product extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            product: this.props.product,
+        };
+    }
+    
     render () {
         return (
         <div className="col-lg-6">
             <div className="card m-2">
-                <div className="text-muted">{this.props.id}.</div>
-                <h5 className="pt-5 border-top">{this.props.name}</h5>
-                <div>$ {this.props.price}</div>
+                <div className="card-body">
+                    <div className="text-muted">
+                        {this.state.product.id}.
+                        <span className="pull-right hand-icon" 
+                            onClick={() => {this.props.onDelete(this.state.product);}}>
+                            <i className="fa fa-times"></i>
+                        </span>
+                    </div>
+                    <h5 className="pt-5 border-top">{this.state.product.name}</h5>
+                    <div>$ {this.state.product.price}</div>
+                </div>
+                <div className="card-footer text-right">
+                    <div className="float-left">
+                        <span className="badge">Amount Left: {this.state.product.amount}</span>
+                        <div className="btn-group">
+                            <button className="btn btn-outline-success" 
+                                onClick={() => {this.props.onIncrement(this.state.product, 10);}}>+</button>
+                            <button className="btn btn-outline-success" 
+                                onClick={() => {this.props.onDecrement(this.state.product, 0);}}>-</button>
+                        </div>
+                    </div>
+                    <div className="float-right">
+                        {this.props.children}
+                    </div>
+                </div>
             </div>
         </div>
         )
+    }
+
+    componentDidMount () {
+
+    }
+
+    componentDidUpdate () {
+
+    }
+
+    componentWillUnmount () {
+        
     }
 }
--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -2,30 +2,79 @@ import React, {Component} from "react";
 import Product from "./Product";
 
 export default class ShoppingCart extends Component {
-    state = {
-        products: [
-            {
-                id: 1, name: "Best Monitor", price: 1, amount: 1
-            },
-            {
-                id: 2, name: "Best Telephone", price: 2, amount: 1
-            },
-            {
-                id: 3,  name: "Not Best Telephone", price: 1, amount: 10
-            }
-        ],
+    constructor(props) {
+        super(props);
+        console.log("cons");
+        this.state = {
+            products: [
+                {
+                    id: 1, name: "Best Monitor", price: 1, amount: 1
+                },
+                {
+                    id: 2, name: "Best Telephone", price: 2, amount: 1
+                },
+                {
+                    id: 3,  name: "Not Best Telephone", price: 1, amount: 10
+                }
+            ],
+        };
     };
+    
     render () {
         return <div className="container-fluid">
             <h4>Shopping Cart</h4>
             <div className="row">
                 {this.state.products.map((product)=>{
-                    return <Product key={product.id}
-                        id={product.id}
-                        name={product.name}
-                        price={product.price}/>
+                    return <Product key={product.id} product={product} 
+                    onIncrement={this.handleIncrement} 
+                    onDecrement={this.handleDecrement}
+                    onDelete={this.handleDelete}> 
+                        <button className="btn btn-primary">Purchease</button>
+                    </Product>
                 })}
             </div>
         </div>
     };
+
+    componentDidMount() {
+        console.log("didm");
+    };
+
+    componentDidUpdate (prevProps, prevState) {
+    };
+
+    componentWillUnmount () {
+
+    };
+
+    componentDidCatch(error, info) {
+        
+    };
+
+    handleIncrement = (product, value) => {
+        let allProducts = [...this.state.products];
+        let i = allProducts.indexOf(product);
+        if (allProducts[i].amount < value) {
+            allProducts[i].amount=value;
+            this.setState({products:allProducts});
+        }
+    };
+
+    handleDecrement = (product, value) => {
+        let allProducts = [...this.state.products];
+        let i = allProducts.indexOf(product);
+        if (allProducts[i].amount>value) {
+            allProducts[i].amount=value;
+            this.setState({products:allProducts});
+        }
+    };
+
+    handleDelete = (product) => {
+        let allProducts = [...this.state.products];
+        let i = allProducts.indexOf(product);
+        if (window.confirm("Are you sure to delete?")) {
+            allProducts.splice(i,1);
+            this.setState({products:allProducts});
+        }
+    }
 }
